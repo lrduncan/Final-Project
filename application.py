2d49efa202ec1cc86e8976191f94ce06be0dbd83
lrduncan <lrduncan@gmail.com>
2017-09-11 16:53:53 +0000

    updated map HTML and JS

--- application.py
+++ application.py
@@ -1,12 +1,18 @@
 from flask import Flask, url_for, render_template, session
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template("index.html")
-    
-@app.route("/login", methods=["GET", "POST"])
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
+@app.route('/map')
+def maps():
+    return render_template("map.html")
