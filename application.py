fb733479cc2a5678c39d81803609c587712a60bc
lrduncan <lrduncan@gmail.com>
2017-09-25 03:10:37 +0000

    Insert into db works, delete uses comments to query. Blank comment entries do not delete

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template, session, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from passlib.apps import custom_app_context as pwd_context
+from passlib.hash import argon2
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
@@ -8,6 +9,8 @@ db = SQLAlchemy(app)
 app.secret_key = "b\x91\x1a\xc8T\x18\x00\x0f\x8f\xa8\\\xa8\xfe\xb6:\xb3\xcc\xe9\xf8&y\\\xa7-"
 
 class User(db.Model):
+    # SQL Alchemy class for users table
+
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -18,6 +21,21 @@ class User(db.Model):
         self.username = username
         self.hashword = hashword
 
+class Trucks(db.Model):
+
+    __tablename__ = 'trucks'
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String(150))
+    destination = db.Column(db.String(150))
+    trailer = db.Column(db.String(30))
+    comments = db.Column(db.String(200))
+
+    def __init__(self, origin, destination, trailer, comments):
+        self.origin = origin
+        self.destination = destination
+        self.trailer = trailer
+        self.comments = comments
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -26,19 +44,25 @@ def index():
 def login():
     session.clear()
     error = None
+
     if request.method == "POST":
         if not request.form.get("username"):
             error = "Username Required"
             return render_template("login.html", error=error)
+
         elif not request.form.get("password"):
             error = "Password Required"
             return render_template("login.html", error=error)
+
         userinfo = User.query.filter_by(username=request.form.get("username")).first()
-        if userinfo == None or not pwd_context.verify(request.form.get("password"), userinfo.hashword):
+
+        if userinfo == None or not argon2.verify(request.form.get("password"), userinfo.hashword):
             error = "Invalid username and/or password"
             return render_template("login.html", error=error)
+
         session["user_id"] = userinfo.id
         return redirect(url_for("index"))
+
     else:
         return render_template("login.html")
 
@@ -48,6 +72,42 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
+def login_required(f):
+    # requires login, from http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect(url_for("login", next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/map')
 def maps():
     return render_template("map.html")
+
+@app.route('/edittrucks', methods=["GET", "POST"])
+@login_required
+def edittrucks():
+
+    if request.method == "POST":
+        if request.form.get("submit"):
+            if not request.form.get("origin") or not request.form.get("destination"):
+                error = "Missing origin and/or destination"
+                return render_template("edittrucks.html", error=error)
+
+            truckinfo = Trucks(request.form.get("origin"), request.form.get("destination"),
+                               request.form.get("trailer"), request.form.get("comments"))
+
+            db.session.add(truckinfo)
+            db.session.commit()
+
+            return redirect(url_for("edittrucks"))
+        elif request.form.get("delete"):
+
+            truckdelete = Trucks.query.filter_by(comments=request.form["delete"]).first()
+            db.session.delete(truckdelete)
+            db.session.commit()
+
+            return redirect(url_for("edittrucks"))
+    trucks = Trucks.query.all()
+    return render_template("edittrucks.html", trucks=trucks)
--- static/mapscripts.js
+++ static/mapscripts.js
@@ -8,17 +8,31 @@ $(function () {
     var canvas = document.getElementById('map');
     var mapOptions = {
       center: {lat: 37.681057, lng: -78.203424},
-      zoom: 12
+      zoom: 9
     };
     map = new google.maps.Map(canvas, mapOptions);
     var marker = new google.maps.Marker({
       position: {lat: 37.681057, lng: -78.203424},
       map: map,
       title: 'Duncan Trucking',
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
+      icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/homegardenbusiness.png'
     });
   }
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
 });
+
+function addMarker(place)
+{
+  var myLatLng = {lat: place.latitude, lng: place.longitude};
+    var icon1 = 'http://maps.google.com/mapfiles/kml/pal2/icon31.png';
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        label: place.place_name + "," + place.admin_name1,
+        map: map,
+        icon: icon1
+    });
+}
